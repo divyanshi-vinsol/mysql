928ddb68d1d8344269c35191ea431371242b0546
divyanshi-vinsol <62337902+divyanshi-vinsol@users.noreply.github.com>
2020-04-01 01:11:30 -0700

    Add files via upload

---
+++ BankExercise/BankTaskAnswers.sql
@@ -0,0 +1,34 @@
+--i) userA is depositing 1000 Rs. his account
+START TRANSACTION;
+UPDATE accounts 
+SET balance=balance+1000 
+WHERE account_no=(
+SELECT account_no 
+FROM users 
+WHERE name="Nick");
+COMMIT;
+--ii) userA is withdrawing 500 Rs.
+START TRANSACTION;
+UPDATE accounts 
+SET balance=balance-500 
+WHERE account_no=(
+SELECT account_no 
+FROM users 
+WHERE name="Nick");
+COMMIT;
+--iii) userA is transferring 200 Rs to userB's account
+START TRANSACTION;
+UPDATE accounts 
+SET balance=balance-200 
+WHERE account_no=(
+SELECT account_no 
+FROM users 
+WHERE name="Nick");
+
+UPDATE accounts 
+SET balance=balance+200 
+WHERE account_no=(
+SELECT account_no 
+FROM users 
+WHERE name="Harry");
+COMMIT;
