cd59ee7065643cd84f75f1f5adba70cfd18db891
divyanshi-vinsol <62337902+divyanshi-vinsol@users.noreply.github.com>
2020-06-08 15:21:15 +0530

    Update BankTaskAnswers.sql

--- BankExercise/BankTaskAnswers.sql
+++ BankExercise/BankTaskAnswers.sql
@@ -1,5 +1,6 @@
 --i) userA is depositing 1000 Rs. his account
 START TRANSACTION;
+LOCK TABLES accounts WRITE, users READ;
 UPDATE accounts 
 SET balance=balance+1000 
 WHERE account_no=(
@@ -7,8 +8,11 @@ SELECT account_no
 FROM users 
 WHERE name="Nick");
 COMMIT;
+UNLOCK TABLES;
+
 --ii) userA is withdrawing 500 Rs.
 START TRANSACTION;
+LOCK TABLES accounts WRITE, users READ;
 UPDATE accounts 
 SET balance=balance-500 
 WHERE account_no=(
@@ -16,8 +20,11 @@ SELECT account_no
 FROM users 
 WHERE name="Nick");
 COMMIT;
+UNLOCK TABLES;
+
 --iii) userA is transferring 200 Rs to userB's account
 START TRANSACTION;
+LOCK TABLES accounts WRITE, users READ;
 UPDATE accounts 
 SET balance=balance-200 
 WHERE account_no=(
@@ -32,3 +39,4 @@ SELECT account_no
 FROM users 
 WHERE name="Harry");
 COMMIT;
+UNLOCK TABLES;
