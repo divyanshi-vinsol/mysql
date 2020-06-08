fb2df9224d53f44e035e9131f06e7e615b7fe9df
divyanshi-vinsol <62337902+divyanshi-vinsol@users.noreply.github.com>
2020-06-08 14:56:12 +0530

    Update employeeCommissionsAnswers.sql

--- EmployeeCommissionExercise/employeeCommissionsAnswers.sql
+++ EmployeeCommissionExercise/employeeCommissionsAnswers.sql
@@ -1,31 +1,41 @@
 --i. Find the employee who gets the highest total commission.
-CREATE VIEW highest_commission AS
-SELECT SUM(commission_amount),employee_id
-FROM commissions 
-GROUP BY employee_id 
-ORDER BY 1 DESC 
+SELECT SUM(commission_amount) AS commission,emp.name 
+FROM employees as emp JOIN commissions as com 
+ON emp.id=com.employee_id 
+GROUP BY com.employee_id
+ORDER BY commission DESC
 LIMIT 1;
+--output
+# commission	name
+9000	Chris Gayle
 
-SELECT Name 
-FROM employees
-WHERE id=(
-SELECT employee_id 
-FROM highest_commission);
 --ii. Find employee with 4th Highest salary from employee table.
 SELECT name 
 FROM Employees 
 ORDER BY salary DESC 
 LIMIT 3, 1;
+--output
+# name
+Rahul Dravid
+
 --iii. Find department that is giving highest commission.
-SELECT Name 
-FROM Departments
-WHERE ID=(SELECT Department_id 
-FROM employees
-WHERE id=(
-SELECT employee_id 
-FROM highest_commission));
+SELECT SUM(commission_amount) AS commission,dept.name
+FROM employees as emp JOIN commissions as com JOIN departments as dept
+ON emp.id=com.employee_id and dept.id=emp.id
+GROUP BY com.employee_id
+ORDER BY commission DESC
+LIMIT 1;
+
+--output
+# commission	name
+9000	Banking
+
 --iv. Find employees getting commission more than 3000
 SELECT commission_amount, GROUP_CONCAT(name) 
-FROM employees e JOIN commissions c ON c.employee_id=e.id 
+FROM employees as emp JOIN commissions as com ON com.employee_id=emp.id 
 WHERE commission_amount>3000 
-GROUP BY 1;
+GROUP BY commission_amount;
+--output
+# commission_amount	GROUP_CONCAT(name)
+4000	Rahul Dravid,Chris Gayle
+5000	Chris Gayle,Wasim Akram
