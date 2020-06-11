1fc257b0f31d4213f925c15b13db20dd9af3ff5c
divyanshi-vinsol <62337902+divyanshi-vinsol@users.noreply.github.com>
2020-06-11 08:23:37 +0530

    Update employeeCommissionsAnswers.sql

--- EmployeeCommissionExercise/employeeCommissionsAnswers.sql
+++ EmployeeCommissionExercise/employeeCommissionsAnswers.sql
@@ -20,22 +20,23 @@ Rahul Dravid
 
 --iii. Find department that is giving highest commission.
 SELECT SUM(commission_amount) AS commission,dept.name
-FROM employees as emp JOIN commissions as com JOIN departments as dept
-ON emp.id=com.employee_id and dept.id=emp.id
-GROUP BY com.employee_id
+FROM employees AS emp JOIN commissions AS com JOIN departments AS dept
+ON emp.id=com.employee_id AND dept.id=emp.id
+GROUP BY emp.department_id
 ORDER BY commission DESC
 LIMIT 1;
 
 --output
 # commission	name
-9000	Banking
+13000	Banking
 
 --iv. Find employees getting commission more than 3000
-SELECT commission_amount, GROUP_CONCAT(name) 
-FROM employees as emp JOIN commissions as com ON com.employee_id=emp.id 
-WHERE commission_amount>3000 
-GROUP BY commission_amount;
+SELECT com.commission_amount, GROUP_CONCAT(name) AS NAME
+FROM employees as emp JOIN commissions as com ON com.employee_id=emp.id
+GROUP BY com.commission_amount 
+HAVING com.commission_amount>3000;
 --output
-# commission_amount	GROUP_CONCAT(name)
-4000	Rahul Dravid,Chris Gayle
+# commission_amount	NAME
+4000	Chris Gayle,Rahul Dravid
 5000	Chris Gayle,Wasim Akram
+
