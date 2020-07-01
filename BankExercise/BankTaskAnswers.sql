c222382ced1b2d4d4318563b2f3fca3ccc07c6a0
divyanshi-vinsol <62337902+divyanshi-vinsol@users.noreply.github.com>
2020-07-01 09:47:01 +0530

    Update BankTaskAnswers.sql

--- BankExercise/BankTaskAnswers.sql
+++ BankExercise/BankTaskAnswers.sql
@@ -1,30 +1,56 @@
 --i) userA is depositing 1000 Rs. his account
 START TRANSACTION;
-LOCK TABLES accounts WRITE, users READ;
+
+SELECT * FROM users 
+WHERE name="Nick" LOCK IN SHARE MODE;
+
+SELECT * FROM accounts WHERE account_no=(
+SELECT account_no 
+FROM users 
+WHERE name="Nick") FOR UPDATE;
+
 UPDATE accounts 
 SET balance=balance+1000 
 WHERE account_no=(
 SELECT account_no 
 FROM users 
 WHERE name="Nick");
+
 COMMIT;
-UNLOCK TABLES;
+
 
 --ii) userA is withdrawing 500 Rs.
 START TRANSACTION;
-LOCK TABLES accounts WRITE, users READ;
+
+SELECT * FROM users 
+WHERE name="Nick" LOCK IN SHARE MODE;
+
+SELECT * FROM accounts WHERE account_no=(
+SELECT account_no 
+FROM users 
+WHERE name="Nick") FOR UPDATE;
+
 UPDATE accounts 
 SET balance=balance-500 
 WHERE account_no=(
 SELECT account_no 
 FROM users 
 WHERE name="Nick");
+
 COMMIT;
-UNLOCK TABLES;
+
 
 --iii) userA is transferring 200 Rs to userB's account
 START TRANSACTION;
-LOCK TABLES accounts WRITE, users READ;
+
+SELECT * FROM users 
+WHERE name="Nick" AND name="Harry" LOCK IN SHARE MODE;
+
+SELECT * FROM accounts WHERE account_no IN(
+SELECT account_no 
+FROM users 
+WHERE name="Nick" AND name="Harry") FOR UPDATE;
+
 UPDATE accounts 
 SET balance=balance-200 
 WHERE account_no=(
@@ -38,5 +64,5 @@ WHERE account_no=(
 SELECT account_no 
 FROM users 
 WHERE name="Harry");
+
 COMMIT;
-UNLOCK TABLES;
